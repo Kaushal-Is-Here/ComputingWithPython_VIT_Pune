a1e4b7767579d237af7c27ca1aace6aa898aec88
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-07-07 23:04:32 +0530

    Add files via upload

---
+++ 09.py
@@ -0,0 +1,8 @@
+#Write a Python program that accepts a hyphen-separated sequence of words as input and prints the words in a hyphen-separated sequence after sorting them alphabetically
+print( '-'.join ( sorted ( list ( input().split('-') ) ) ) )
+
+#***************************************************** Output **************************************************************************/
+
+
+#green-red-yellow-black-white
+#black-green-red-white-yellow
