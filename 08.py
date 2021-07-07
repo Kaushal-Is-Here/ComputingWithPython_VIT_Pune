a0916ab401893c35a04a423e5226ce9ff022f7e6
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-07-07 23:01:44 +0530

    Add files via upload

---
+++ 08.py
@@ -0,0 +1,12 @@
+#Write a Python function to multiply all the numbers in a list
+
+def multiply(numbers):  
+    total = 1
+    for n in numbers:
+        total *= n  
+    return total  
+print(multiply((1, 2, 3, 5, 6)))
+
+#***************************************************** Output **************************************************************************/
+
+180
