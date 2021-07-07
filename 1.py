b9e4d8d402635e6019765dafeec138965dd136c6
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-07-07 22:44:51 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+# Create a function showEmployee() in such a way that it should accept employee name, and its salary and display both. If the salary is missing in the function call assign default value 9000 to salary
+
+def showEmployee(name, salary=9000):
+    print("Employee", name, "salary is:", salary)
+
+showEmployee("SIR", 9000)
+showEmployee("SIR")
+
+#Employee SIR salary is: 9000
+#Employee SIR salary is: 9000
---
+++ 2.py
@@ -0,0 +1,21 @@
+#Write a Python function to find the Max of three numbers
+
+
+n1=int(input("Enter first number: "));
+n2=int(input("Enter second number: "));
+n3=int(input("Enter Third number: "));
+def f():
+    if(n1>=n2) and (n1>=n3):
+        l=n1
+    elif(n2>=n1) and (n2>=n3):
+         l=n2
+    else:
+         l=n3
+    print("Largest number among  the three is",l)
+f()
+
+
+#Enter first number: 2
+#Enter second number: 3
+#Enter Third number: 10
+#Largest number among  the three is 10
