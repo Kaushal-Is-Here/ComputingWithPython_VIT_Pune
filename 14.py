8d0f75f7f314633006bdc88c38f55158f0b553e6
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-07-09 22:55:33 +0530

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,11 @@
+#Write a Python program to remove None value from a given list using lambda function.
+#[12, 0, None, 23, None, -55, 234, 89, None, 0, 6, -12]
+
+thislist = ["12", "0", "None", "23" , "None", "-55", "234", "89", "None", "0", "6", "-12"]
+thislist.remove("None")
+print(thislist)
+
+#***************************************************** Output **************************************************************************/
+
+
+#[19, 65, 57, 39, 152, 190]
